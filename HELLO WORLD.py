7b461cb1c2cab88bb3a55942ef443a547af8cf81
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-07 01:04:53 +0530

    Hello World

---
+++ HELLO WORLD.py
@@ -0,0 +1,11 @@
+def d():
+    #script begin
+    print(' *')
+    print(' Good Morning')
+    print(' HELLO WORLD')
+    print(' Good Morning')
+    print(' HELLO WORLD')
+    print(' Good Morning')
+
+d()
+   #script end  
