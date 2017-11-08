4b364cd446abc1514cf9d64bd46c952d7a7ee29e
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-08 23:39:31 +0530

    Assignment values to variables of different data types

---
+++ AssingmentVaraibles.py
@@ -0,0 +1,9 @@
+def d():
+    integerVar = 13 // INTEGER VARIABLES
+    floatVar = 12.98 // FLOAT VARIABLES
+    charVar = "Hello World" // CHARACTER VARIABLES
+    print(integerVar)
+    print(floatVar)
+    print(charVar)
+
+d();    
