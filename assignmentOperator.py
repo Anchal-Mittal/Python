7671c64de8026c142299b2101bbe609ac8108656
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-10 00:40:15 +0530

    Assignment Operators

---
+++ assignmentOperator.py
@@ -0,0 +1,27 @@
+def d():
+    c=10
+    b=3
+    print("ADDITION OF TWO NUMBERS")
+    c += b
+    print(c)
+    print("SUBTRACTION OF TWO NUMBERS")
+    c -= b
+    print(c)
+    print("MULTIPICATION OF TWP NUMBERS" )
+    c *= b
+    print(c)
+    print("DIVISION OF TWO NUMBERS ")
+    c /= b
+    print(c)
+    print("MODE OPERATION OF TWO NUMBERS ")
+    c %= b
+    print(c)
+    print(" FLOOR DIVISION")
+    c //=b
+    print(c)
+    print("POWER OF NUMBER")
+    c **= b
+    print(c)
+    
+d();
+    
