60b3b76bf89901f683c11ab2c0899533c2883d26
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-10 02:12:27 +0530

    Assignment Operators

--- assignmentOperator.py
+++ assignmentOperator.py
@@ -2,25 +2,25 @@ def d():
     c=10
     b=3
     print("ADDITION OF TWO NUMBERS")
-    c += b
+    c +=b
     print(c)
     print("SUBTRACTION OF TWO NUMBERS")
-    c -= b
+    c -=b
     print(c)
     print("MULTIPICATION OF TWP NUMBERS" )
-    c *= b
+    c *=b
     print(c)
     print("DIVISION OF TWO NUMBERS ")
-    c /= b
+    c /=b
     print(c)
     print("MODE OPERATION OF TWO NUMBERS ")
-    c %= b
+    c %=b
     print(c)
     print(" FLOOR DIVISION")
     c //=b
     print(c)
     print("POWER OF NUMBER")
-    c **= b
+    c **=b
     print(c)
     
 d();
