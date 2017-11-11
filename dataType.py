e0afa136142fa67d3ef56c8dec8c5034c8a69d3b
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-11 15:40:18 +0530

    Numeric data types in python

---
+++ dataType.py
@@ -0,0 +1,9 @@
+def loop():
+    print("check long and double data types are not exist")   
+    print(type(2**3)) # int data type
+    print(type(2**38))# int data type not long
+    print(type(2*3.9)) #float data type
+    print(type(3.8**50))# float data type not double 
+
+loop();
+    
