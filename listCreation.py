86f6ff465cf5615cdbd40e613dee7818fb26bd60
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-12 11:29:21 +0530

    Creating list and use its inbuilt functions

---
+++ listCreation.py
@@ -0,0 +1,25 @@
+def d():
+    print("CREATING THE LIST : ")
+    list1 =[1,0,4,-2,3]
+    list2 =['a','4','S','d','f']
+    print(list1)
+    print(list2)
+    print("length of list1=")
+    print(len(list1))
+    print("length of list2=")
+    print(len(list2))
+    print("maximum elements of list2")
+    print(max(list2))
+    print("minimum elements of list2")
+    print(min(list1))
+    del list1[3]
+    print(list1)
+    print("length of list1 after updatation ")
+    print(len(list1))
+    print(list1[1:])
+    seq= 1,3, 4,2
+    list(seq)
+    print(len(seq))
+    print(seq)
+d();
+    
