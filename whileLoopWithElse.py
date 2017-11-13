102751a27f30dbc3975b85df381c52584c9d9a5a
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-14 00:51:58 +0530

    While loop with else statement

---
+++ whileLoopWithElse.py
@@ -0,0 +1,12 @@
+
+def d():
+      var =1
+      while(var <5) :
+          print("the var is ", var)
+          var=var +1
+      else :
+           print("the var is not less than ",var)
+d(); 
+    
+
+    
