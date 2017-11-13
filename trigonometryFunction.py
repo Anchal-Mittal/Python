730b8400e6c5122218d4d9b950089d3a55a80f08
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-14 00:43:01 +0530

    Trigonometry Function

---
+++ trigonometryFunction.py
@@ -0,0 +1,19 @@
+import math
+
+def d():
+       x=1;
+       y=math.sqrt(24)
+       print(math.acos(x))# arc cosine
+       print(math.asin(x))# arc sine
+       print(math.atan(x))# arc tan
+       print(math.cos(x))# cos value
+       print(math.sin(x))#sin value
+       print(math.tan(x))#tan value
+       print(math.hypot(x,y))#return sqrt(x*x+y*y)
+       print(math.atan2(y,x))#tan(y/x) in radian
+       print(math.degrees(x))#convert x into degree
+       print(math.radians(x))#convert x into radian
+d();
+    
+
+    
