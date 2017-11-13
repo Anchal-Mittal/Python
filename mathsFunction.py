2c69a1cfff0815c89ffa783bde60dffaf4a77af2
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-14 00:08:25 +0530

    Maths Function

---
+++ mathsFunction.py
@@ -0,0 +1,14 @@
+def d():
+    x =5
+    y=8.9
+    print(float(x)) #printing int as a float
+    print(x)# print as a int
+    print(int(y))#printing float as int
+    print(complex(x))# print as complex with 0 as imaginary parts
+    print(complex(x,y))#take x as a real part and y as complex part and print as a complex number
+    print(abs(y))
+    
+d();
+    
+
+    
