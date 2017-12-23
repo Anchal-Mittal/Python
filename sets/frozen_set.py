ea3391470b568f7c80f604e7e65d77a425a44966
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-12-23 23:19:15 +0530

    Frozen set

---
+++ sets/frozen_set.py
@@ -0,0 +1,10 @@
+def fun():
+    frozen_set=frozenset(["e","g","j"])
+    print(frozen_set)
+    #frozen_set.add("k") can not add in frozen set
+fun();
+"""
+Frozen Sets Frozen sets are immutable objects that only support methods and 
+operators that produce a result without a?ecting the frozen set or sets to which
+they are applied.
+"""
