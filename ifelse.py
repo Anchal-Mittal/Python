f777ca88a5743d0fd659eebe9fb4e741fc092466
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-12-23 20:05:58 +0530

    if else statement

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+def ifelseFun():
+    a=10
+    b=11
+    if a%b==0 :
+        print("a and b are not prime to each other")
+    else:
+        print("a and b are prime to each other")      
+ifelseFun();              
