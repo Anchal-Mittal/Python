29307bf0a32a0453adc790754a3b613fd1abc792
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-12-24 12:59:10 +0530

    reading input from console

---
+++ inputOutput/input1.py
@@ -0,0 +1,11 @@
+from sys import stdin,stdout
+def inputTaking():
+    print("enter one num1")
+    n=input()
+    print(n)
+    print("enter a line")
+    m=stdin.readline()
+    stdout.write(m)
+inputTaking();    
+    
+    
