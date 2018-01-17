26f650d316cd2502359e7b64e981357488740dad
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-01-17 23:35:28 +0530

    graph ploating

---
+++ graph/graph1.py
@@ -0,0 +1,13 @@
+def fun():
+    
+    import matplotlib.pyplot as plt
+    x=[1,2,4]
+    y=[2,5,8]
+    plt.plot(x,y)
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.title("initial graph")
+    plt.show()
+
+fun()    
+    
