850352fdd551a0d4d4e32a986b4cb7e1bdfd2ec2
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-01-25 23:55:18 +0530

    create window

---
+++ tkinter/tkinter1.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+def main():
+    m=tkinter.tk()
+    m.title("WELCOME")
+    button=Button(m,text="NAME")
+    m.pack()
+    m.mainloop()
+if __name__=='_main_':
+    main()
+    
