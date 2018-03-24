ac7ff55735ce2bcccb2049aa583c62614cc9c234
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-03-24 23:21:36 +0530

    increment and decrement operator does not work

---
+++ increment.py
@@ -0,0 +1,19 @@
+
+
+def d():
+
+      var =1
+
+      while(var <5) :
+
+          print("the var is ", var)
+
+          var++#no increment and decrement operator in python
+
+      else :
+
+           print("the var is not less than ",var)
+
+d(); 
+
+    
