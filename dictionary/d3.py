0a26ad0fb65d59f12d2e94d94b7f490bb8360944
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-03-25 02:48:45 +0530

    Create d3.py

---
+++ dictionary/d3.py
@@ -0,0 +1,13 @@
+def fun():
+
+  dict={'NAME':'ANCHAL','ROLL_NO' :'4','CONTACT_NO':'8750957330'}
+  print(dict['NAME'])
+  print(dict['ROLL_NO'])
+  print(dict['CONTACT_NO'])
+  dict['NAME']='NEHA'
+  print(dict['NAME'])
+  dict1={'subject':'comp.sci','sem':'3','area':'local','subject':'comme'}
+  print(dict1['subject'])#print the last subject value comme not comp.sci
+  print(len(dict1))
+  print(str(dict1))
+fun()    
