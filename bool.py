533d8c8061dd011fd7b670849c6ae41087a83741
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-03-24 22:52:49 +0530

    boolean

---
+++ bool.py
@@ -0,0 +1,15 @@
+def fun():
+    a=True
+    b=False
+    print(a)
+    print(b)
+    a=not True
+    b=not False
+    print(a)
+    print(b)
+    print(~a)
+    print(~b)
+    a=9
+    print(~a)#print(-a-1)
+    
+fun()
