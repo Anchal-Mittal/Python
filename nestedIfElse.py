be2c4024432a6d312c683a9f94b5b806c62e651c
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-03-24 23:02:58 +0530

    nested if

---
+++ nestedIfElse.py
@@ -0,0 +1,17 @@
+def fun():
+    a=10
+    b=11
+    if a!=b :
+        if a>b :
+            print(a)
+        else:
+            print(b)
+    else :
+        print("a and b are equal")
+
+fun();        
+
+
+            
+
+            
