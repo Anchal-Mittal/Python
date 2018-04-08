2b5147c762be22c06327fec14653d920751a5fff
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-04-08 22:02:28 +0530

    print the specific month calender of given month no

---
+++ calender/c3.py
@@ -0,0 +1,11 @@
+#print the specific month calender of given month no
+import calendar
+def fun():
+    print(calendar.month(2018,6,6,1))  
+fun();    
+
+"""
+ month (year, month, w, l) :- This function prints the month of a specific year mentioned in arguments.
+ It takes 4 arguments, year, month, width of characters and no. of lines taken by a week.
+ 
+"""
